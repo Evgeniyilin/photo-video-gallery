fa025cf9ab0a77a03aff76088faed5ab4a0b69aa
EvgeniyIlin <evgeniy@live.ru>
2022-09-13 00:54:12 +0500

    add components and grid style

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Gallery.module.css';
+
+const Gallery = () => {
+    return (
+        <div className={s.gallery}>
+            Gallery
+        </div>
+    );
+};
+
+export default Gallery;
